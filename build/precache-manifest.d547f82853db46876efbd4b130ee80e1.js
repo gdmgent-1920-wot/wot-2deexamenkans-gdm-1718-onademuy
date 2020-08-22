1e0011656b6b0fa454a4f691ca60b182ff95f862
Ona Demuytere <onademuy@student.arteveldehs.be>
2020-08-22 20:56:19 +0200

    build

---
+++ build/precache-manifest.d547f82853db46876efbd4b130ee80e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a10b71317f8131c013e83a1fa6618d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85fd0fe0c805fca20c4c",
+    "url": "/static/css/main.fc78691b.chunk.css"
+  },
+  {
+    "revision": "55d15960bfe9730ff98e",
+    "url": "/static/js/2.dbe97fc0.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.dbe97fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85fd0fe0c805fca20c4c",
+    "url": "/static/js/main.95640175.chunk.js"
+  },
+  {
+    "revision": "734d276d8947f7f331e3",
+    "url": "/static/js/runtime-main.a96080ae.js"
+  },
+  {
+    "revision": "114fbe4f8c1782922542384c803a40c0",
+    "url": "/static/media/droplet-filled.114fbe4f.svg"
+  }
+]);
